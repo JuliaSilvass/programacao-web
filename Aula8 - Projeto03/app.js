b8d0459ec2e179254353513fa604217ae0aac480
JuliaSilvass <juliasilvass01@gmail.com>
2025-04-07 19:42:27 -0300

    Inicio projeto 03

--- Aula8 - Projeto03/app.js
+++ Aula8 - Projeto03/app.js
@@ -6,8 +6,26 @@ const exphbs = require('express-handlebars')
 
 app.engine('.hbs', exphbs.engine({extname: '.hbs', defaultLayout: 'main'}))
 
-app.set('view engine', 'handlebars')
+app.set('view engine', 'hbs')
+
+const tarefas = []
+let id = 1
+
+app.get('/', function(req, res){
+    res.render('cadastro')
+})
+
+app.get('/adicionarTarefa', function(req, res){
+    const novaTarefa = {
+        id: id++, 
+        titulo: titulo, 
+        descricao: descricao, 
+        concluida: false,
+    }
+
+    tarefas.push(novaTarefa)
+})
 
 app.listen(port, servIp, function (){
-    console.log(``)
+    console.log(`\n Aplicação web executando em http://${servIp}:${port}`)
 })
