c94bf2d9b171161670456247d3044e01f72aaa08
JuliaSilvass <juliasilvass01@gmail.com>
2025-04-15 21:48:01 -0300

    projeto 03

--- Aula8 - Projeto03/app.js
+++ Aula8 - Projeto03/app.js
@@ -5,8 +5,8 @@ const servIp = '127.0.0.1'
 const exphbs = require('express-handlebars')
 
 app.engine('.hbs', exphbs.engine({extname: '.hbs', defaultLayout: 'main'}))
-
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
 const tarefas = []
 let id = 1
@@ -15,15 +15,34 @@ app.get('/', function(req, res){
     res.render('cadastro')
 })
 
-app.get('/adicionarTarefa', function(req, res){
+app.post('/adicionarTarefa', function(req, res){
+
+    console.log('BODY:', req.body)
+
     const novaTarefa = {
         id: id++, 
-        titulo: titulo, 
-        descricao: descricao, 
+        titulo: req.body.nome, 
+        descricao: req.body.descricao,
+        responsavel: req.body.responsavel,
+        status: req.body.status,
+        data_termino: req.body.data_termino, 
         concluida: false,
     }
 
-    tarefas.push(novaTarefa)
+    // const hoje = new Date().toDateString()
+    
+    // console.log(novaTarefa.data_termino)
+    // console.log(dataFormatada)
+    
+    // // if (novaTarefa.data_termino === dataFormatada){
+    // // } else {
+    // //     res.send(`Não é possivel cadastrar data de execução máxima no passado!`)
+    // // }
+
+        tarefas.push(novaTarefa)
+        res.send(`tarefa feita`)
+
+
 })
 
 app.listen(port, servIp, function (){
